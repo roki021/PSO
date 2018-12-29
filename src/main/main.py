85995201a20bd8560ecb284526ef5f6b781efca5
roki021 <rodusekvladimir@yahoo.com>
2018-12-29 18:31:39 +0100

    Kreiranje inicijalnog stanja

--- src/main/main.py
+++ src/main/main.py
@@ -4,6 +4,13 @@ Created on Dec 29, 2018
 @author: Vlada
 '''
 
+from numpy.random import uniform
+from numpy import multiply
+
 if __name__ == '__main__':
-    print ("MILANEEE")
-    pass
+   # velocity = list(uniform(-1, 1, 60))
+   # print (velocity)
+   #for t in range(1, 100):
+   #    x = 0.4 + ((0.9-0.4)/(100-0))*(100-t);
+   #    print (x)
+   pass
--- src/optimization/PSO.py
+++ src/optimization/PSO.py
@@ -3,3 +3,27 @@ Created on Dec 29, 2018
 
 @author: Vlada
 '''
+
+from optimization.ann_criterion import optimality_criterion
+from numpy.random import uniform
+
+cp_start = 2.5;   # inicijalna vrednost kognitivnog faktora
+cp_end   = 0.5;   # finalna vrednost kognitivnog faktora
+cg_start = 0.5;   # inicijalna vrednost socijalnog faktora
+cg_end   = 2.5;   # finalna vrednost socijalna faktora 
+w_start  = 0.9;   # inicijalna vrednost inercijalnog faktora
+w_end    = 0.4;   # finalna vrednost inercijalnog faktora
+
+class Particle(object):
+    def __init__(self, w):
+        self.position = w[:]
+        self.velocity = list(uniform(-1, 1, 60))
+        self.peronal_best = self.position[:]
+        self.feval = optimality_criterion(self.position)
+        self.feval_best = self.feval
+    
+    
+class PSO(object):     
+    def __init__(self):
+        pass
+        
