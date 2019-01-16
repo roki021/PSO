350d5e5c6e23cbbc475733b5b9994f7768089d8a
roki021 <rodusekvladimir@yahoo.com>
2019-01-16 21:43:09 +0100

    Minorne izmene
    Prebaceno je sve u numpy ndarray klasu i dodati su komentari.

--- src/main/main.py
+++ src/main/main.py
@@ -2,4 +2,5 @@ from optimization.ann_criterion import optimality_criterion
 from optimization.PSO import PSO
 
 if __name__ == '__main__':
-    print(PSO(optimality_criterion))
+    a = PSO(optimality_criterion)
+    print(a[0], "\n", a[1])
--- src/optimization/PSO.py
+++ src/optimization/PSO.py
@@ -1,37 +1,47 @@
 from optimization.ann_criterion import optimality_criterion
 import numpy as np 
 
-# inicijalne opcije algoritma
-options = { 'max_it'       : 100,
-            'particle_num' : 10,
-            'dim'          : 60,
-            'bounds'       : (-10, 10), 
-            'cp_start'     : 2.5,   # inicijalna vrednost kognitivnog faktora
-            'cp_end'       : 0.5,   # finalna vrednost kognitivnog faktora
-            'cg_start'     : 0.5,   # inicijalna vrednost socijalnog faktora
-            'cg_end'       : 2.5,   # finalna vrednost socijalna faktora 
-            'w_start'      : 0.9,   # inicijalna vrednost inercijalnog faktora
-            'w_end'        : 0.4 }   # finalna vrednost inercijalnog faktora}
+
+# opcije algoritma
+options = { 'max_it'       : 100,       # broj iteracija
+            'particle_num' : 30,        # broj cestica
+            'dim'          : 60,        # dimenzija
+            'bounds'       : (-10, 10), # opseg nasumicnih pocetnih vrednosti za svaku dimenziju
+            'cp_start'     : 2.5,       # startna vrednost kognitivnog faktora
+            'cp_end'       : 0.5,       # finalna vrednost kognitivnog faktora
+            'cg_start'     : 0.5,       # startna vrednost socijalnog faktora
+            'cg_end'       : 2.5,       # finalna vrednost socijalna faktora 
+            'w_start'      : 0.9,       # startna vrednost inercijalnog faktora
+            'w_end'        : 0.4 }      # finalna vrednost inercijalnog faktora
+
 
 class Particle(object):
+    """
+        Klasa predstavlja jednu cesticu roja
+        Sadrzi sve potrebne atribute vezane za cesticu
+    """
+    
     def __init__(self, w):
-        self.position = np.array(w)
-        self.velocity = list(np.random.uniform(-1, 1, 60))
-        self.peronal_best = self.position[:]
+        self.position = np.copy(w)
+        self.velocity = np.random.uniform(-1, 1, len(w))
+        self.peronal_best = self.position
         self.feval = None
         self.feval_best = None
         
     def evalute(self, func):
+        """
+            Racuna vrednost trenutne pozicije tacke u prosledjenoj funkciji
+        """
         self.feval = func(self.position)
         
         if self.feval_best is None or self.feval < self.feval_best:
             self.feval_best = self.feval
-            self.peronal_best = self.position
-        #elif self.feval < self.feval_best:
-        #    self.feval_best = self.feval
-        #    self.peronal_best = self.position
-    
+            self.peronal_best = np.copy(self.position)
+            
     def update_data(self, swarm_best_pos, iteration, opt):
+        """
+            Azurira sve atribute za datu iteraciju
+        """
         w = interpolation(opt['w_start'], opt['w_end'], opt['max_it'], 0, iteration)
         cp = interpolation(opt['cp_start'], opt['cp_end'], opt['max_it'], 0, iteration) 
         cg = interpolation(opt['cg_start'], opt['cg_end'], opt['max_it'], 0, iteration) 
@@ -41,19 +51,22 @@ class Particle(object):
         
         cognitive = np.multiply(cp * rp, np.subtract(self.peronal_best, self.position))
         social = np.multiply(cg * rg, np.subtract(swarm_best_pos, self.position))
-        v = np.add(np.multiply(w, self.velocity), np.add(cognitive, social))
-        self.velocity = v.tolist()
+        self.velocity = np.add(np.multiply(w, self.velocity), np.add(cognitive, social))
         
-        p = np.add(self.position, self.velocity)
-        self.peronal_best = p.tolist()
+        self.peronal_best = np.add(self.position, self.velocity)
+    
     
 def PSO(func, opt = options):
+    """
+        Izvrsava algoritam optimizacije rojem cestica,
+        Trazi minimum za prosledjenu funkciju
+    """
     swarm = []
     swarm_best = None
-    swarm_best_pos = []
+    swarm_best_pos = np.full((opt['dim'], ), 0) 
     
     for i in range(opt['particle_num']):
-        swarm.append(Particle(list(np.random.uniform(-10, 10, 60))))
+        swarm.append(Particle(np.random.uniform(-10, 10, opt['dim'])))
     
     i = 0
     while i < opt['max_it']:
@@ -69,8 +82,11 @@ def PSO(func, opt = options):
             
         i += 1
             
-    
-    return swarm_best_pos, swarm_best
+    return np.array(swarm_best_pos), swarm_best
+        
         
 def interpolation(start, end, max_it, min_it, curr_it):
-    return start + ((end - start) / (max_it - min_it)) * (max_it - curr_it);
+    """
+        Interpolira izmenu za prosledjeni faktor
+    """
+    return start + ((end - start) / (max_it - min_it)) * (max_it - curr_it)
